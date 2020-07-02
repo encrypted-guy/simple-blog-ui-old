efe203f2da95688342b94bdda31574c7fe0d238f
encrypted_guy <nafiu.developer@gmail.com>
2020-07-03 03:50:25 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,64 @@
+window.sr = ScrollReveal();
+
+sr.reveal('.navbar', {
+    duration: 2000,
+    origin: 'bottom'
+});
+
+sr.reveal('.short', {
+    duration: 2000,
+    origin: 'bottom',
+    distance: '300px'
+});
+/*profile*/
+
+sr.reveal('.profile-row', {
+    duration: 2000,
+    origin: 'left',
+    distance: '300px',
+    viewFactor: 0.2
+});
+
+/*blog */
+sr.reveal('.blog-view-left', {
+    duration: 2000,
+    origin: 'left',
+    distance: '300px',
+    viewFactor: 0.2
+});
+sr.reveal('.blog-view-right', {
+    duration: 2000,
+    origin: 'right',
+    distance: '300px',
+    viewFactor: 0.2
+});
+sr.reveal('.blog-card', {
+    duration: 2000,
+    origin: 'top',
+    distance: '300px'
+});
+/*service*/
+sr.reveal('.card-a', {
+    duration: 2000,
+    origin: 'left',
+    distance: '300px'
+});
+sr.reveal('.card-b', {
+    duration: 2000,
+    origin: 'top',
+    distance: '300px'
+});
+sr.reveal('.card-c', {
+    duration: 2000,
+    origin: 'right',
+    distance: '300px'
+});
+/* contact */
+sr.reveal('#contact', {
+    duration: 2000,
+    origin: 'left',
+    distance: '300px'
+});
+
+
+
